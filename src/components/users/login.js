df05debdba553d4e1c60a8a027918d80196a3a9f
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-19 17:18:05 +0300

    Created the basic jumbotron for the login page.

---
+++ src/components/users/login.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() { 
+        return (
+            <div className="jumbotron">
+                <div className="card-body" id="login-card">
+
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default Login;
