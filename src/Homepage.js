9d803eb92023ec1cf596c0ba5ffe77fb6cd6073b
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-20 17:21:09 +0300

    Imported the BrowserRouter, Route and Switch modules.

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 class Homepage extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
-        
+
     }
 
     render() { 
