eb8e0145994c55f12905ae62d0862cb16a6fb119
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-20 17:34:38 +0300

    Created a basic navbar component with react routing.

--- src/components/index/Navbar.js
+++ src/components/index/Navbar.js
@@ -1,1 +1,29 @@
 import React, {Component} from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+           <div>
+               <Navbar bg="primary" fixed="top">
+                    <Nav>
+                        <LinkContainer to="/">
+                            <Nav.Link>
+                                Home
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>
+                                Login
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+               </Navbar>
+           </div>     
+        );
+    }
+}
+ 
+export default Navbar;
