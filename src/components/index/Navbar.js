d8de3b36e15783752f27139815c60675b4a3cee6
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-21 14:55:38 +0300

    Created a Main component.

--- src/components/index/Navbar.js
+++ src/components/index/Navbar.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+
 import './Navbar.css';
+import Bell from '../content/notifications/Bell';
 
 class Navmenu extends Component {
     state = {  }
     render() { 
         return ( 
            <div className="jumbotron" id="navbar">
-               <Navbar bg="primary" fixed="top">
+               {/* <Navbar bg="primary" fixed="top">
                     <Container>
                     <Nav>
                         <LinkContainer to="/">
@@ -23,7 +25,43 @@ class Navmenu extends Component {
                         </LinkContainer>
                     </Nav>
                     </Container>
-               </Navbar>
+               </Navbar> */}
+
+                <Navbar bg="primary" expand="lg" fixed="top">
+                    <Container>
+                        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+                        <Bell />
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link>
+                                    Home
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    Login
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/signup">
+                                <Nav.Link>
+                                    Sign up
+                                </Nav.Link>
+                            </LinkContainer>
+                        <NavDropdown title={<i class="fas fa-bars fa-2x"></i>} id="basic-nav-dropdown" style={{margin: "0 3rem"}}>
+                            <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                         
+                        </Nav>
+                      </Navbar.Collapse>
+                 </Container>
+                </Navbar>
            </div>     
         );
     }
