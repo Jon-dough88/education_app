8375eb3e6c58c9691a525e39eff71ab55c0e8b36
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-26 10:16:59 +0300

    Updated the Main component.

--- src/components/index/Main.js
+++ src/components/index/Main.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
+
 import LessonCard from '../content/lessons/LessonCard';
+import HistoryContainer from '../content/history/HistoryContainer';
+import SuggestionCard from '../content/suggestions/SuggestionCard';
 
 class Main extends Component { 
 
     render(){
         return (
-            <div className="container jumbotron">
+            <div className="jumbotron">
                 
                 <div className="row" id="history">
                     <div className="col">
-
+                        <HistoryContainer />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col" id="lessons">
                         <LessonCard />
+                      
+                        
                     </div>
                 </div>
                 <div className="row">
                     <div className="col" id="suggestions">
-
+                        <SuggestionCard />
                     </div>
                 </div> 
 
