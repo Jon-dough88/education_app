95dd02c23c092e12ce634cb4960875904b85664f
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-26 10:15:27 +0300

    Added a SuggestionTab component.

---
+++ src/components/content/suggestions/SuggestionTab.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+class SuggestionTab extends Component {
+
+    render() {
+
+        return(
+            <div className="col-2">
+                <div className="card suggestion-tab">
+                
+                </div>    
+            </div>
+        )
+    }
+}
+
+
+export default SuggestionTab
