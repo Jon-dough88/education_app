2a6dc4ced9c76f73d9ceeaefbf08e2de685697a8
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-26 10:15:06 +0300

    Added a SuggestionCard component.

---
+++ src/components/content/suggestions/SuggestionCard.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import SuggestionTab from './SuggestionTab';
+import './Suggestion.css'
+
+
+class SuggestionCard extends Component {
+
+    render() {
+        return(
+            <div className="card suggestion-row">
+                <div className="row g-0">
+                    <div className="col-sm-12">
+                        <div className="card-body">
+                           <div className="suggestion-title"><span><h2 className="card-title">Recommended For You</h2></span></div>
+                           <br></br> 
+                           <div className="container">
+                                <div className="row">
+                                    <SuggestionTab />
+                                    <SuggestionTab />
+                                    <SuggestionTab />
+                                    <SuggestionTab />
+                                    <SuggestionTab />
+                                    <SuggestionTab />
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        )
+    }
+}
+
+
+export default SuggestionCard
