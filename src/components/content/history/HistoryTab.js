706758644d248eec150d42e43cf9b2e3c0ae6fb1
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-07-26 10:12:57 +0300

    Created the HistoryTab.js file.

---
+++ src/components/content/history/HistoryTab.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './History.css';
+
+class HistoryTab extends Component {
+
+    render() {
+        return (
+            <div className="col-2">
+                <div className="circular-container">
+                    <h1 className="history-content">GR</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default HistoryTab
