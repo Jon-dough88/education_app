d50530fda5b08c96cf257f32eee7be11825b7f1e
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-01 14:52:53 +0300

    Created the basic layout for the GroupMenu component.

---
+++ src/components/content/dropdown/groups/GroupsMenu.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class GroupMenu extends Component {
+
+    render() {
+        return (
+            <div className="container jumbotron">
+                <div className="row" id="group-title-row">
+                    <div className="col-md-8">
+                        <h1>My Groups</h1>
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
+                <div className="row">
+                    
+                </div>  
+            </div>
+        )
+    }
+}
+
+
+export default GroupMenu
