a126fee303fff4516e5bb9fffba756a513e96aea
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-11 13:30:49 +0300

    Created a Slide component.

---
+++ src/components/elements/Slider/Slider.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Slider extends Component {
+    state = {  }
+    
+    render() { 
+        return ( 
+            <div className="row slider-container">
+                
+            </div>
+         );
+    }
+}
+ 
+export default Slider;
