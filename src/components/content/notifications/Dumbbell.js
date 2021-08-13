90af82a7183c7ded334296c63c7fe29f79a0d5e0
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-13 13:32:15 +0300

    Changed "class" to "className" (Dumbbell.js, 10).

--- src/components/content/notifications/Dumbbell.js
+++ src/components/content/notifications/Dumbbell.js
@@ -7,7 +7,7 @@ class Dumbbell extends Component {
     render() {
         return (
             <div>
-                <i class="fas fa-dumbbell fa-2x dumbbell"></i>
+                <i className="fas fa-dumbbell fa-2x dumbbell"></i>
             </div>
         )
     }
