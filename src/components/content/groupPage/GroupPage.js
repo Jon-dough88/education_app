91b1537d773ef35f6933c7940507788354c8b512
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-20 11:46:06 +0300

    Created the basic layout of the group page.

---
+++ src/components/content/groupPage/GroupPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+
+class GroupPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="jumbotron container">
+                <div className="row">
+                    <div className="col group-title">
+                        {/* <h1>{{GroupName}}</h1> */}
+                        <h1>Group Name</h1>
+                    </div>
+                    <div className="row">
+                        <div className="col group-btn-container">
+                            <button className="btn btn-lg btn-primary">Manage Group</button>
+                            <button className="btn btn-lg btn-primary">Group Profile</button>
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2>Group History</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col" id="group-history"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2>Recommended</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col" id="group-recommendations">
+                            
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2>Saved</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col" id="group-saved-items">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default GroupPage;
