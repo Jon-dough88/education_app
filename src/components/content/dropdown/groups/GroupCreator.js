a9a385d571684a2abee0eca04938a8ee299b5e23
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-20 11:31:48 +0300

    Created a group page component.

--- src/components/content/dropdown/groups/GroupCreator.js
+++ src/components/content/dropdown/groups/GroupCreator.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import './Groups.css'
 
 class GroupCreator extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className="card">
+            <div className="card group-creation-menu">
                 <div className="card-body">
                     <form className="form-inline justify-content-center" id="group-creation-form">
-                        <div className="form-group">
+                        <div className="form-group groups-formGroup">
                             <label for="group-name">Group name: </label>
-                            <input type="text" className="form-control" id="group-name" aria-describedby="group-name"></input>
+                            <input type="text" className="form-control groups-input" id="group-name" aria-describedby="group-name"></input>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group groups-formGroup">
                             <label for="student-search">Add a student (click on the searchbar to choose): </label>
-                            <input type="search" id="student-search" className="form-control"></input>
+                            <input type="search" id="student-search" className="form-control groups-input"></input>
                             <button className="btn btn-primary btn-lg">Add</button>
                         </div>
                         <div className="form-group">
@@ -21,7 +22,7 @@ class GroupCreator extends Component {
                             <button className="btn-lg level-btn" >5p</button>
                         </div>
                         <div className="form-group">
-                            <button type="submit" className="btn btn-primary btn-lg">Create Group</button>
+                            <button type="submit" className="btn btn-primary btn-lg create-group-btn">Create Group</button>
                         </div>
                     </form>
                 </div>
