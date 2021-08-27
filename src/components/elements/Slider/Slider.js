6440ccbad20b249a65d157c603b5d02c639cdcbf
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-27 10:31:34 +0300

    Undid the changes to the state.

--- src/components/elements/Slider/Slider.js
+++ src/components/elements/Slider/Slider.js
@@ -9,25 +9,23 @@ class Slider extends Component {
         super(props);
 
         this.state=
-        // [
-        {
-            item1: {
+        [
+            {
                 id: 0,
                 subject: "Grammar",
                 image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.technocrazed.com%2Fwp-content%2Fuploads%2F2018%2F09%2Fgrammar.jpg&f=1&nofb=1"
             },
-            item2:{
+            {
                 id: 1,
                 subject: "Reading comprehension",
                 image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.huffingtonpost.com%2F2016-06-05-1465146534-8456759-readingabook.jpeg&f=1&nofb=1"
             },
-            item3: {
+            {
                 id: 2,
                 subject: "Writing",
                 image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200416164101-3-underscored-creative-writing-lead-super-tease.jpg&f=1&nofb=1"
-            }
-        // ]
-        }
+            },
+        ]
     }
 
 
