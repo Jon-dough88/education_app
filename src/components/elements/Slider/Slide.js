f4dddfcfd7cb0e902bbda46fedd6e1016bb1a629
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-08-27 09:52:23 +0300

    Copied the methods from the Slide component to the Slider component.

--- src/components/elements/Slider/Slide.js
+++ src/components/elements/Slider/Slide.js
@@ -8,10 +8,10 @@ class Slide extends Component {
         super();
 
         this.state={
-            isDragging: false,
-            startPos: 0,
+            // isDragging: false,
+            // startPos: 0,
             // currentTranslate: 0,
-            currentIndex: 0,
+            // currentIndex: 0,
             // currentPosition: 0,
             // prevTranslate: 0,
             // animationId: 0,
@@ -19,135 +19,142 @@ class Slide extends Component {
         }
     }
 
-    cancelDragEffect(event){
-        event.preventDefault();
-    }
+    // cancelDragEffect(event){
+    //     event.preventDefault();
+    // }
 
-    // // Touch event methods
+    // // // Touch event methods
 
-    handleTouchEvent(event, index) {
-         console.log("Touch started!")
-        //  console.log(event.type)
-         console.log(`Start position: ${this.state.startPos}`)
+    // handleTouchEvent(event, index) {
+    //      console.log("Touch started!")
+    //     //  console.log(event.type)
+    //      console.log(this.props.index)
 
-         this.setState({
-            currentIndex: index,
-            isDragging: true,
-            
-         })
+    //     const startPosition = this.getPositionX(event)
 
-         this.getPosition(event)
+    //      this.setState({
+    //         currentIndex: index,
+    //         isDragging: true,
+    //         startPos: startPosition
+    //      })
 
-         this.animate()
+    //      console.log(`Start position: ${this.state.startPos}`)
 
-         
-         
+    //      this.animate()
+   
         
-    }
-
-
-    handleTouchMove(e) {
-           
-        // const currentPos = this.getPosition(e);
-
-        if(this.state.isDragging === true) {
-            console.log("Touch move")
-            console.log(`Current position: ${this.state.currentPosition}`)
-
-            this.setState({
-                correntPosition: this.getPosition(e),
-                currentTranslate: this.state.prevTranslate + this.state.currentPosition - this.state.startPos
-            })
-            
-
-            // this.state.currentTranslate = this.state.prevTranslate + this.state.currentPosition - this.state.startPos
-        }
-    }
-
-    handleTouchEnd() {
-        console.log("end")
-        this.setState({
-            isDragging: false
-        })
-    }
-
-    getPosition(event){
-        console.log(event)
-        this.setState({
-            startPos: this.getCurrentPosition(event),
-            currentPosition: this.getCurrentPosition(event)
-        })
-    
-    }
-
-    getCurrentPosition(event) {
-        // console.log(event)
-        const currentPos = event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
-        // console.log(`Start position: ${startPos}`)
-        return currentPos
-    }
-
-    
-    animate() {
-
-        this.state.isDragging === true &&
-            this.setState({
-                animationId: window.requestAnimationFrame(this.animate()),
-                transform: `translateX(${this.state.currentTranslate}px)`
-            })
-
-        console.log(this.state.currentTranslate)
-    }
-
-    // animation() {
-    //     console.log('The animation has been triggered!')
-    //     this.state.isDragging === true &&
-    //     requestAnimationFrame(this.animate())
     // }
 
 
-    handleMouseDown(event, index) {
-        console.log("Touch started!")
-         this.setState({
-             currentIndex: index
-         })
-         console.log(event.type)
-         this.setState({
-             isDragging: true
-         })
-    }
+    // handleTouchMove(e) {
+           
+    //     // const currentPos = this.getPosition(e);
+
+    //     if(this.state.isDragging === true) {
+    //         console.log("Touch move")
+
+    //         console.log(`Previous translate: ${this.state.prevTranslate}`)
+    //         console.log(`Current position: ${this.state.currentPosition}`)
+    //         console.log(`Start position: ${this.state.startPos}`)
+            
+
+    //         const translate = this.state.prevTranslate + this.state.currentPosition - this.state.startPos 
+            
+
+
+    //         this.setState({
+    //             correntPosition: this.getCurrentPosition(e),
+    //             currentTranslate: translate
+    //         })
+            
+    //         console.log(`Current translate: ${translate}`)
+    //         // this.state.currentTranslate = this.state.prevTranslate + this.state.currentPosition - this.state.startPos
+    //     }
+    // }
+
+    // handleTouchEnd() {
+    //     console.log("end")
+    //     this.setState({
+    //         isDragging: false
+    //     })
+    // }
+
+    // getCurrentPosition(event){
+        
+    //     this.setState({
+    //         currentPosition: this.getPositionX(event)
+    //     })
+    
+    // }
+
+    // getPositionX(event) {
+    //     const positionX = event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+    //     return positionX
+    // }
 
     
+    // animate() {
+
+    //     // const transform = `translateX(${this.state.currentTranslate}px)`;
+    //     this.setSliderPosition()
+        
+    //     this.state.isDragging === true &&
+            
+    //         this.setState({
+    //             animationId: window.requestAnimationFrame(this.animate()),
+    //             // transform: `translateX(${this.state.currentTranslate}px)`
+    //         })
+
+    //     console.log(`Transform value: ${this.state.transform}`)
+    // }
+
+    // setSliderPosition() {
+    //    this.setState({
+    //        transform: `translateX(${this.state.currentTranslate}px)`
+    //    }) 
+    // }
+
 
     
-
 
     render() { 
         
-        // console.log(this.props.index)
+
 
         return ( 
-            <div className="slide" key={this.props.id} index={this.props.index} style={{transform: `${this.state.transform}`}}> 
+            <div className="slide" key={this.props.id} 
+                index={this.props.index} 
+                // style={{transform: `${this.state.transform}`}}
+                // onDragStart={(e) => {this.cancelDragEffect(e)}} 
+                // onTouchStart={(event, index) => {this.handleTouchEvent(event, index)}}
+                // onTouchEnd={() => {this.handleTouchEnd()}}
+                // onTouchMove={(e) => {this.handleTouchMove(e)}}
+                    
+                // onMouseDown={(event, index) => {this.handleTouchEvent(event, index)}}
+                // onMouseUp={() => {this.handleTouchEnd()}}
+                // onMouseLeave={() => {this.handleTouchEnd()}}
+                // onMouseMove={(e) => {this.handleTouchMove(e)}}
+                // onContextMenu={(e) => {e.preventDefault()}}
+                > 
+                
                 <div className="demo-content">
                     {/* <h2>{this.props.subject}</h2> */}
                     <img className="demo-image" alt="Lesson" 
                     // style={{transform: `${this.state.transform}`}}
                     src={this.props.image} 
-                    onDragStart={(e) => {this.cancelDragEffect(e)}} 
-                    onTouchStart={(event, index) => {this.handleTouchEvent(event, index)}}
-                    onTouchEnd={() => {this.handleTouchEnd()}}
-                    onTouchMove={(e) => {this.handleTouchMove(e)}}
+                    // onDragStart={(e) => {this.cancelDragEffect(e)}} 
+                    // onTouchStart={(event, index) => {this.handleTouchEvent(event, index)}}
+                    // onTouchEnd={() => {this.handleTouchEnd()}}
+                    // onTouchMove={(e) => {this.handleTouchMove(e)}}
                     
-                    onMouseDown={(event, index) => {this.handleTouchEvent(event, index)}}
-                    onMouseUp={() => {this.handleTouchEnd()}}
-                    onMouseLeave={() => {this.handleTouchEnd()}}
-                    onMouseMove={(e) => {this.handleTouchMove(e)}}
-                    onContextMenu={(e) => {
-                        e.preventDefault();
-                    
-                    }}
+                    // onMouseDown={(event, index) => {this.handleTouchEvent(event, index)}}
+                    // onMouseUp={() => {this.handleTouchEnd()}}
+                    // onMouseLeave={() => {this.handleTouchEnd()}}
+                    // onMouseMove={(e) => {this.handleTouchMove(e)}}
+                    // onContextMenu={(e) => {e.preventDefault()}}
+                    >
 
-                    ></img>
+                    </img>
                 </div>
             </div>
          );
