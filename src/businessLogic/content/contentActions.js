ec6d517e40f03d37bcb177d0cab0a08bac4d4832
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-02 11:33:20 +0300

    Added commented description of the various actions.

---
+++ src/businessLogic/content/contentActions.js
@@ -0,0 +1,12 @@
+// Creating a new lesson/recommendation
+
+
+// Editing the lesson's details
+
+
+// Sending students lessons/notifications
+
+
+// Providing students with feedback
+
+
---
+++ src/businessLogic/users/usersActions.js
@@ -0,0 +1,15 @@
+// Creating a new user
+
+
+// Editing account details
+
+
+
+// Changing account settings
+
+
+
+// Login
+
+
+// Logout
