a338d717ed592ce377c7b5fa514b91cacec4a542
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-02 11:46:08 +0300

    Imported the group reducer withing the appStore.

--- src/businessLogic/appStore/appStore.js
+++ src/businessLogic/appStore/appStore.js
@@ -3,10 +3,11 @@ import thunk from 'redux-thunk';
 
 import usersReducer from '../users/usersReducer';
 import contentReducer from '../content/contentReducer';
+import groupReducer from '../groups/groupReducer';
 
 const appStore = createStore(combineReducers({
     users: usersReducer,
-    content: contentReducer
+    content: contentReducer,
 }), applyMiddleware(thunk))
 
 
