49de7260483db7ff0a30536664e4c087a3cfe328
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-02 12:17:31 +0300

    imported the formReducer and added it to the appStore.

--- src/businessLogic/appStore/appStore.js
+++ src/businessLogic/appStore/appStore.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 import usersReducer from '../users/usersReducer';
 import contentReducer from '../content/contentReducer';
@@ -8,7 +9,9 @@ import groupReducer from '../groups/groupReducer';
 const appStore = createStore(combineReducers({
     users: usersReducer,
     content: contentReducer,
-    groups: groupReducer
+    groups: groupReducer,
+    form: formReducer
+    
 }), applyMiddleware(thunk))
 
 
