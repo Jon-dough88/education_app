be0c31981fe1df4f2aa7adbed6682532c9781aa2
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-02 11:44:40 +0300

    Added commented actions in the groupActions file.

---
+++ src/businessLogic/groups/groupActions.js
@@ -0,0 +1,21 @@
+// Creating a new group
+
+
+
+// Editing a group's details
+
+
+
+// Adding group history item
+
+
+
+// Adding recommended
+
+
+
+// Adding saved item
+
+
+
+// Group notifications
