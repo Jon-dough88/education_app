e706a82f11790b214c9a39170fc0333dcf1eda23
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-30 10:20:03 +0300

    Added logic for a user-related reducer.

--- src/components/users/Signup/Signup.js
+++ src/components/users/Signup/Signup.js
@@ -77,80 +77,16 @@ let Signup = props => {
     )
 }
 
+// const mapDispatchToProps = dispatch => {
+//     return {
+
+//     }
+// }
+
 Signup = reduxForm({
     form: 'signup'
 })(Signup)
-// class Signup extends Component {
 
-//     constructor(props){
-//         super(props);
-
-        
-//     }
-
-
-//     render() {
-//         return (
-//             <div className="jumbotron">
-//                 <div className="card" >
-//                     <div className="card-body" id="signup-body">
-//                         <form className="form-inline justify-content-center" id="signup-form">
-//                             <div className="form-group">
-//                                 <input className="form-control form-control-lg" 
-//                                 type="text" 
-//                                 id=""
-//                                 // ref={}
-//                                 placeholder="First Name"
-//                                 >    
-//                                 </input>
-//                             </div>
-//                             <div className="form-group">
-//                                 <input className="form-control form-control-lg" 
-//                                 type="text" 
-//                                 id=""
-//                                 // ref={}
-//                                 placeholder="Last Name"
-//                                 > 
-//                                 </input>
-//                             </div>
-//                             <div className="form-group">
-//                                  <input className="form-control form-control-lg" 
-//                                 type="email" 
-//                                 id=""
-//                                 // ref={}
-//                                 placeholder="Email"
-//                                 >    
-//                                 </input>
-//                             </div>
-//                             <div className="form-group"> 
-//                                 <input className="form-control form-control-lg" 
-//                                 type="password" 
-//                                 id=""
-//                                 // ref={}
-//                                 placeholder="Password"
-//                                 >
-//                                 </input>
-//                             </div>
-//                             <div className="form-group">
-//                                 <input className="form-control form-control-lg" 
-//                                 type="password" 
-//                                 id=""
-//                                 // ref={}
-//                                 placeholder="Retype your password"
-//                                 >    
-//                                 </input> 
-
-//                             </div>
-//                             <input className="btn btn-primary btn-lg signup-button" type="submit" value="Sign up!"></input> 
-//                             <br />   
-//                             <a href="/login"><strong style={{fontSize: "18px"}}>Already a member? Click here!</strong></a>    
-//                         </form>
-//                     </div>
-//                 </div>     
-//             </div>
-//         )
-//     }
-// }
 
 
 export default Signup
--- src/components/users/Signup/SignupPage.js
+++ src/components/users/Signup/SignupPage.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
 import Signup from '../Signup/Signup';
+import { signup } from '../../../businessLogic/users/usersActions';
+
 
 class SignupPage extends Component {
     
     submit = values => {
         console.log(values)
+        this.props.userRegistered(values)
+    }
+
+    mapDispatchToProps = dispatch => {
+        return {
+            userRegistered: (values) => dispatch(signup(values))
+        }
     }
 
     
