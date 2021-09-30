39df13b40a302b9947d1c74dc886a080614596a0
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-09-30 10:22:42 +0300

    Changed the url reference in the usersActions to userUrl/signup (line 14).

---
+++ src/businessLogic/users/usersReducer.js
@@ -0,0 +1,21 @@
+import * as ACTIONS from './usersActions';
+
+const initialState = {
+    currentUser: null,
+    errors: []
+}
+
+
+const usersReducer = (state = initialState, action) => {
+    switch(action.type) {
+        
+        // case ACTIONS.USER_LOGIN
+        //     return
+        
+
+        default:
+            return state
+    }
+}
+
+export default usersReducer
