0ed914b94cbf1d54fb7a7df572fbd8ba193a2167
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-07 23:20:10 +0300

    Created the basic outline of the LessonPage.

---
+++ src/components/content/lessons/LessonPage/LessonInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class LessonInfo extends React.Component {
+    render() { 
+        return (
+            <div className="jumbotron">
+                
+            </div>
+        )
+    }
+}
+ 
+export default LessonInfo;
