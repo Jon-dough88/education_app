9f5be9118a60f139d30b67149e991ea6d08f0736
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-11 00:17:12 +0300

    Moved the .lesson-image class tag to the lesson image.

--- src/components/content/lessons/LessonPage/LessonInfo.js
+++ src/components/content/lessons/LessonPage/LessonInfo.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 import './LessonInfo.css';
+// import LessonCard from '../LessonCard';
 
 
-class LessonInfo extends React.Component {
+class LessonInfo extends Component {
     render() { 
         return (
             <div className="jumbotron">
                 <div className="row title-row">
                     <div className="col">
-                        <div className="lesson-image">
-
+                        <div>
+                            <img className="lesson-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.nSSaVLnXxl2ivUfvn8I9rQHaEl%26pid%3DApi&f=1" alt="Lesson image"></img>
                         </div>
                     </div>
                     <div className="col">
