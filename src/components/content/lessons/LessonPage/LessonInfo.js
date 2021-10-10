473032cf8ee6fad4dc5a47e0f48fb258236eb38d
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-11 00:21:03 +0300

    Changed the main div's className from jumbotron to card.

--- src/components/content/lessons/LessonPage/LessonInfo.js
+++ src/components/content/lessons/LessonPage/LessonInfo.js
@@ -7,11 +7,14 @@ import './LessonInfo.css';
 class LessonInfo extends Component {
     render() { 
         return (
-            <div className="jumbotron">
+            <div className="card">
                 <div className="row title-row">
                     <div className="col">
-                        <div>
+                        <div className="image">
                             <img className="lesson-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.nSSaVLnXxl2ivUfvn8I9rQHaEl%26pid%3DApi&f=1" alt="Lesson image"></img>
+                        </div>
+                        <div className="level">
+                            <h2>B2</h2>
                         </div>
                     </div>
                     <div className="col">
