65ebc164f63e08dfc49d4473c127223875682b00
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-11 01:34:08 +0300

    Added a title prop to the LessonInfo state.

--- src/components/content/lessons/LessonPage/LessonInfoTag.js
+++ src/components/content/lessons/LessonPage/LessonInfoTag.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 
 
 class LessonInfoTag extends Component {
+
     constructor(props) {
         super(props);
-        this.state = {  }
+        
     }
     render() { 
         return ( 
             <div className="col info-tag">
                 <div className="row">
-                    <div className="col">
-
+                    <div className="col info-image">
+                        <img src={this.props.image} alt="Lesson info image"></img>
                     </div>
-                    <div className="col">
+                    <div className="col info-title">
+                        <div className="row">
 
+                        </div>
                     </div>
                 </div>    
             </div>
