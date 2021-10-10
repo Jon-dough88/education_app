8d0298e7b1864e3e1437b7b80e281f15c9cf2f75
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-11 02:14:15 +0300

    Created a lessonInfoTag css file.

--- src/components/content/lessons/LessonPage/LessonInfo.js
+++ src/components/content/lessons/LessonPage/LessonInfo.js
@@ -18,12 +18,27 @@ class LessonInfo extends Component {
                 package: "basic",
                 text: "Basic",
                 tags: ["Vocabulary 4","Grammar2", "Reading 2"],
-                info: 
-                    {
-                        title: "Intro",
-                        thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/LED_traffic_light.svg/220px-LED_traffic_light.svg.png"
-                    }
                 
+            
+                info: {
+                    intro: 
+                        {
+                            title: "Intro",
+                            thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/LED_traffic_light.svg/220px-LED_traffic_light.svg.png"
+                        },
+                    
+                    vocabulary: 
+                        {
+                            title: "Pre 1: Vocabulary",
+                            thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2FdtatfDRuD_CvqhWlZPdFhO8ZVaY%3D%2F2x0%3A638x424%2F1200x800%2Ffilters%3Afocal(2x0%3A638x424)%2Fcdn.vox-cdn.com%2Fassets%2F1797435%2F4093455287_211bbb4a2e_z.jpg&f=1&nofb=1"
+                        },
+                    main: 
+                        {
+                            title: "Main 1: Reading",
+                            thumbnail: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.huffingtonpost.com%2F2016-06-05-1465146534-8456759-readingabook.jpeg&f=1&nofb=1"
+                        }
+
+                }
                 
            
         }
@@ -81,13 +96,8 @@ class LessonInfo extends Component {
                 </div>
 
                 <div className="row content-row">
-                    <LessonInfoTag title={this.state.info.title} thumbnail={this.state.info.thumbnail} />
-                      {/* {this.state.map((infoTab) => (
-                          <LessonInfoTag 
-                            // image={infoTab.intro.url}
-                            title={infoTab.title}    
-                          />
-                      ))}           */}
+                    <LessonInfoTag title={this.state.info.intro.title} thumbnail={this.state.info.intro.thumbnail} />
+                    <LessonInfoTag title={this.state.info.vocabulary.title} thumbnail={this.state.info.vocabulary.thumbnail} />  
                 </div>
             </div>
         )
