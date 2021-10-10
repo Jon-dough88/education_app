e9e7ccbbc02b25e167c1a27143022008f6f51924
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-11 01:16:16 +0300

    Created a LessonInfoTag component.

---
+++ src/components/content/lessons/LessonPage/LessonInfoTag.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class LessonInfoTag extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+        );
+    }
+}
+ 
+export default LessonInfoTag;
