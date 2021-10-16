d57ea167c9d9a23b86383d318cb26566fc138f3d
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-16 12:50:52 +0300

    Removed the curly braces around signupValues (usersActions, 17).

--- src/businessLogic/users/usersActions.js
+++ src/businessLogic/users/usersActions.js
@@ -14,7 +14,7 @@ export const USER_SIGNED_UP = 'users/signup'
 
 export const signup = (signupValues) => async dispatch => {
     console.log(signupValues)
-   await axios.post(`${usersUrl}/signup`, {signupValues})
+   await axios.post(`${usersUrl}/signup`, signupValues)
    .then((response) => {
        console.log(response)
        dispatch({type: USER_SIGNED_UP, message: "Sign up successful"})
