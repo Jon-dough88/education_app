7eaa4f0da339c68f41e4adf429a398c68279053d
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-09 15:38:27 +0200

    Copied validationField into the validations file.

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,1 +1,19 @@
 export const required = value => value ? undefined : 'Please fill in this field.'
+
+
+export const validationField = ({input, label, type, meta: {touched, error, warning}}) => (
+
+
+    
+    <div className="validation-field-container">
+        {/* <label>{label}</label> */}
+        <div >
+            <input 
+            className="input" {...input} placeholder={label} type={type} 
+            ></input>    
+        </div>
+        <div className="error-container">
+            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+)
