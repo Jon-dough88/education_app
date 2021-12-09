704670bac17a5c4bb729e896cac56502c27fa54d
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-09 15:40:05 +0200

    Added "export" to all functions.

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,17 +1,17 @@
 export const required = value => value ? undefined : 'Please fill in this field.'
 
-const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+export const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
 'Invalid email address' : undefined
 
 
-const maxLength = max => value => value && value.length > max ? `The maximum number of characters is ${max}!` : undefined 
-const maxLengthValue = maxLength(18);
-const emailMaxValue = maxLength(25);
+export const maxLength = max => value => value && value.length > max ? `The maximum number of characters is ${max}!` : undefined 
+export const maxLengthValue = maxLength(18);
+export const emailMaxValue = maxLength(25);
 
-const minLength = min => value => value && value.length < min ? `Must be at least ${min} characters!` : undefined
-const minLengthValue = minLength(2)
+export const minLength = min => value => value && value.length < min ? `Must be at least ${min} characters!` : undefined
+export const minLengthValue = minLength(2)
 
-const comparePasswords = (value, allValues) => value !== allValues.password ? 'The passwords don\'t match' : <span><p className="match-message">Password match!</p></span>
+export const comparePasswords = (value, allValues) => value !== allValues.password ? 'The passwords don\'t match' : <span><p className="match-message">Password match!</p></span>
 
 
 export const validationField = ({input, label, type, meta: {touched, error, warning}}) => (
