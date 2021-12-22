3e4fa8985078e16ce695cf51b7d793b1a3cf00ac
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-22 15:15:20 +0200

    Created the fetchGroup method.

--- src/businessLogic/groups/groupActions.js
+++ src/businessLogic/groups/groupActions.js
@@ -1,5 +1,15 @@
+import axios from 'axios';
+
+const groupUrl = 'http://localhost:4000/api/groups';
+
+export const GROUP_FETCH_SUCCESS = 'groups/fetchSuccess';
+export const GROUP_FETCH_FAILURE = 'groups/fetchFailure';
+
+
+
 // Creating a new group
 
+export const fetchGroups = () => async dispatch => {}
 
 
 // Editing a group's details (grade, level, )
