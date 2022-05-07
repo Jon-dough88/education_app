d8fb90198cff659c877f8f59cf07264cc667eb9c
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-05-07 17:56:19 +0300

    Added ._id to the action payload at groupReducer.

--- src/businessLogic/groups/groupReducer.js
+++ src/businessLogic/groups/groupReducer.js
@@ -45,7 +45,7 @@ const groupReducer = (state = initialState, action) => {
             return { ...state, studentListFetched: false }
 
         case ACTIONS.STUDENT_LIST_FETCHED:
-            return { ...state, studentListFetched: true, studentList: action.payload }
+            return { ...state, studentListFetched: true, studentList: action.payload._id }
         
         case ACTIONS.STUDENT_LIST_FETCHED:
             return { ...state, studentListFetched: false, errors: action.payload.errors}
