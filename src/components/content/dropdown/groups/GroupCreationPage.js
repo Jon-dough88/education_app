42213a94e96816d77158e5cd37613c5afce15929
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-05-17 17:48:39 +0300

    Changed the reference to GroupCreator.

---
+++ src/components/content/dropdown/groups/GroupCreationPage.js
@@ -0,0 +1,40 @@
+import React, {Component } from 'react';
+import { connect } from 'react-redux';
+
+import  { GroupCreator }
+
+class GroupCreationPage extends Component {
+
+submit = values => {
+
+}
+
+render(){
+    return(
+
+    )
+}
+ 
+}
+
+
+const mapStateToProps = state => {
+    return {
+        userName: state.users.userName,
+        userId: state.users.userId,
+        groupNameExists: state.groups.groupNameExists,
+        message: state.groups.message,
+        studentList: state.groups.studentList
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // getUser: () => dispatch(fetchUser()),
+        newRefreshToken: () => dispatch(getRefreshToken()),
+        checkForGroup: (userId, groupName) => dispatch(findGroupName(userId, groupName)),
+        getStudentList: (userId, userName) => dispatch(fetchStudentList(userId, userName))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroupCreationPage)
