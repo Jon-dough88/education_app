c8f114d6fd0f65ee5dff3758b5f3ebd3ce919b42
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-06-05 20:06:22 +0300

    Changed "handleSubmit" to "onSubmit" (GroupCreator 67, 76).

--- src/components/content/dropdown/groups/GroupCreationPage.js
+++ src/components/content/dropdown/groups/GroupCreationPage.js
@@ -7,85 +7,85 @@ import { createNewGroup } from '../../../../businessLogic/groups/groupActions';
 import GroupCreator from './GroupCreator';
 
 
+// class GroupCreationPage extends Component {
+
+//     submit = (values) => {
+//         console.log(values)
+//     }
+
+//     render() {
+//         return(
+//             <GroupCreator onSubmit = {this.submit} />
+//         )
+//     }
+// }
+
+
+// export default GroupCreationPage
+
 class GroupCreationPage extends Component {
 
-    submit = (values) => {
-        console.log(values)
+
+    componentDidMount(){
+
+        // this.props.getUser();
+        this.props.newRefreshToken();
+        
+        console.log(`Username is: ${this.props.userName}`)
     }
 
-    render() {
-        return(
-            <GroupCreator onSubmit = {this.submit} />
-        )
+
+    handleKeyPress(e) {
+        let searchValue = e.target.value;
+        console.log(`Search value: ${searchValue}`);
+
+
+        this.props.checkForGroup(this.props.userId, searchValue);
+
+        
     }
+
+    handleStudentList(e) {
+        const {userId, userName} = this.props;
+        console.log(`User ID: ${userId}. User name: ${userName}`);
+        // console.log("Input clicked!");
+        this.props.getStudentList(userId, userName)
+    }
+
+
+submit = (values) => {
+    // this.handleStudentList(values);
+    // this.handleKeyPress(values)
+    console.log(values)
+    // this.props.createGroup(values)
+
+}
+
+render(){
+    return <GroupCreator onSubmit={this.submit}  />
+}
+ 
 }
 
 
-export default GroupCreationPage
+const mapStateToProps = state => {
+    return {
+        userName: state.users.userName,
+        userId: state.users.userId,
+        groupNameExists: state.groups.groupNameExists,
+        message: state.groups.message,
+        studentList: state.groups.studentList
+    }
+}
 
-// class GroupCreationPage extends Component {
+const mapDispatchToProps = dispatch => {
+    return {
+        // getUser: () => dispatch(fetchUser()),
+        newRefreshToken: () => dispatch(getRefreshToken()),
+        checkForGroup: (userId, groupName) => dispatch(findGroupName(userId, groupName)),
+        getStudentList: (userId, userName) => dispatch(fetchStudentList(userId, userName)),
+        createGroup: (values) => dispatch(createNewGroup(values))
+    }
+}
 
-
-//     componentDidMount(){
-
-//         // this.props.getUser();
-//         this.props.newRefreshToken();
-        
-//         console.log(`Username is: ${this.props.userName}`)
-//     }
-
-
-//     handleKeyPress(e) {
-//         let searchValue = e.target.value;
-//         console.log(`Search value: ${searchValue}`);
-
-
-//         this.props.checkForGroup(this.props.userId, searchValue);
-
-        
-//     }
-
-//     handleStudentList(e) {
-//         const {userId, userName} = this.props;
-//         console.log(`User ID: ${userId}. User name: ${userName}`);
-//         // console.log("Input clicked!");
-//         this.props.getStudentList(userId, userName)
-//     }
-
-
-// submit = (values) => {
-//     // this.handleStudentList(values);
-//     // this.handleKeyPress(values)
-//     console.log(values)
-//     // this.props.createGroup(values)
-
-// }
-
-// render(){
-//     return <GroupCreator onSubmit={this.submit}  />
-// }
- 
-// }
-
-
-// const mapStateToProps = state => {
-//     return {
-//         userName: state.users.userName,
-//         userId: state.users.userId,
-//         groupNameExists: state.groups.groupNameExists,
-//         message: state.groups.message,
-//         studentList: state.groups.studentList
-//     }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         // getUser: () => dispatch(fetchUser()),
-//         newRefreshToken: () => dispatch(getRefreshToken()),
-//         checkForGroup: (userId, groupName) => dispatch(findGroupName(userId, groupName)),
-//         getStudentList: (userId, userName) => dispatch(fetchStudentList(userId, userName)),
-//         createGroup: (values) => dispatch(createNewGroup(values))
-//     }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(GroupCreationPage)
+export default connect(mapStateToProps, mapDispatchToProps)(GroupCreationPage)
