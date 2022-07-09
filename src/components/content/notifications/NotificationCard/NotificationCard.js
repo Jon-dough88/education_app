c76ed07c0f697d2c2e54d20fb4556b3b272c5fa8
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-07-09 19:51:51 +0300

    Created a notifications card component.

---
+++ src/components/content/notifications/NotificationCard/NotificationCard.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+
+class NotificationCard extends Component {
+    
+    state = {  } 
+    render() { 
+        return (
+            <div className="row">
+                    <div className="col col-md-12">
+                        <div className="notification-card">
+                             
+                        </div>
+                    </div>
+            </div>
+        );
+    }
+}
+ 
+export default NotificationCard;
--- src/components/content/notifications/NotificationsPage/NotificationPage.js
+++ src/components/content/notifications/NotificationsPage/NotificationPage.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 
 
 class NotificationPage extends Component {
-    
+
     state = {  } 
 
     render() { 
         return (
             <div className="jumbotron container">
-                <h1>Notifications Page</h1>
+                {/* <div className="row">
+                    <div className="col col-md-12">
+                        <div className="card">
+
+                        </div>
+                    </div>
+                </div> */}
             </div>
         );
     }
