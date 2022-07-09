0ec525494dfd96def2d109b5797231f673d99861
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-07-09 19:44:40 +0300

    Created a NotificationsPage component.

---
+++ src/components/content/notifications/NotificationsPage/NotificationPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class NotificationPage extends Component {
+    
+    state = {  } 
+
+    render() { 
+        return (
+            <div className="jumbotron container">
+                <h1>Notifications Page</h1>
+            </div>
+        );
+    }
+}
+ 
+export default NotificationPage;
