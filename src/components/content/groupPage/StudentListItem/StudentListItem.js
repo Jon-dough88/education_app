0be8fe6fdf1b8d2b09e7297c4191a763e979e385
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-08-07 14:38:01 +0300

    Importred StudentListItem into the GroupModal component.

---
+++ src/components/content/groupPage/StudentListItem/StudentListItem.js
@@ -0,0 +1,19 @@
+import React, { component } from 'react';
+
+class StudentListItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+
+
+    render() { 
+        return ( 
+           <div className="card">
+               
+           </div>     
+        );
+    }
+}
+ 
+export default StudentListItem;
